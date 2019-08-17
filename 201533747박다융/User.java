422357ad7110a149106654127eefd8883029819b
qkrekdbd <49428961+qkrekdbd@users.noreply.github.com>
2019-08-17 13:59:11 +0900

    201533747 박다융 자바 게시판 프로그램 과제 제출합니다
    이상입니다

---
+++ 201533747박다융/User.java
@@ -0,0 +1,20 @@
+package javabasic.week2.project;
+
+public class User {
+	private String name;
+
+	
+	public User(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+}
